87f9b4a5cc917c3897f38598f2cf2f3174061ee7
TomtheCoder2 <jan.wilhem77@gmail.com>
2023-06-14 19:03:29 +0200

    some improvements?

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,56 @@ use std::time::Instant;
 
 struct Checker {
     checked: Vec<bool>,
+    checked_min: usize,
 }
 
 impl Checker {
     fn new(n: usize) -> Self {
         Checker {
-            checked: vec![false; n]
+            checked: vec![false; n],
+            checked_min: 0,
         }
     }
 
-    fn check(&mut self, mut n: u32) -> bool {
+    fn check(&mut self, n: u32) -> bool {
+        let r = self.check_internal(n);
+        if !r {
+            println!(
+                "found a number that doesnt return to the normal 1-2-4 loop: {}",
+                n
+            );
+            false
+        } else if self.checked.len() > n as usize - self.checked_min {
+            for _ in 0..n as usize - self.checked_min {
+                self.checked.remove(0);
+            }
+            self.checked_min = n as usize;
+            true
+        } else {
+            true
+        }
+    }
+
+    fn check_internal(&mut self, mut n: u32) -> bool {
         match n {
             0 | 1 | 2 | 4 => true,
             _ => {
                 while n & (1 << 0) == 0 {
                     n /= 2;
                 }
-                if n == 1 || (self.checked.len() > n as usize && self.checked[n as usize]) {
+                if n == 1
+                    || (n as usize > self.checked_min
+                        && self.checked.len() > n as usize - self.checked_min
+                        && self.checked[n as usize - self.checked_min])
+                    || n < self.checked_min as u32
+                {
                     true
-                } else if self.checked.len() > n as usize {
-                    let r = self.check(3 * n + 1);
-                    self.checked[n as usize] = r;
+                } else if self.checked.len() > n as usize - self.checked_min {
+                    let r = self.check_internal(3 * n + 1);
+                    self.checked[n as usize - self.checked_min] = r;
                     r
                 } else {
-                    self.check(3 * n + 1)
+                    self.check_internal(3 * n + 1)
                 }
             }
         }
@@ -35,13 +61,16 @@ impl Checker {
 // without cache: 4ns for 100
 // with cache: 71ns for 100
 fn main() {
-    let n = 27;
-    let mut checker = Checker::new(100000);
-    let n = 1000000;
-    benchmark(move |i| {
-        // println!("checking: {}", i);
-        checker.check(i as u32);
-    }, n as usize);
+    let _n = 27;
+    let mut checker = Checker::new(10000);
+    let n = 10000;
+    benchmark(
+        move |i| {
+            // println!("checking: {}", i);
+            checker.check(i as u32);
+        },
+        n as usize,
+    );
     let mut checker = Checker::new(100000);
 
     let start = Instant::now();
@@ -52,17 +81,16 @@ fn main() {
     println!("Total time: {:?}", duration);
     println!("Average time: {:?}", duration / n);
 
-    println!("check n={} -> {}", n, checker.check(n));
+    println!("check n={} -> {}", n, checker.check_internal(n));
 }
-
 
 /// Benchmark a function
 /// # Arguments:
 /// * `f`(false, n)
 /// * `n` - The number of times to run the function
 pub fn benchmark<F>(mut f: F, n: usize)
-    where
-        F: FnMut(usize) + Send + 'static,
+where
+    F: FnMut(usize) + Send + 'static,
 {
     let start = Instant::now();
     for i in 0..n {
